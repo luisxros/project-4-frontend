5baf254789153d8a7652adcf366c9f12bd860320
Luis Rosario <luisalmonter@gmail.com>
2021-01-11 23:53:49 -0500

    adds audio

--- src/components/Audio.js
+++ src/components/Audio.js
@@ -1,20 +1,20 @@
-// import React, { Component } from "react"
-// import '../App.css'
+import React, { Component } from "react"
+import '../App.css'
 
 
 
-// export default class extends Component {
-//   playAudio() {
-//     const audioEl = document.getElementsByClassName("audio-element")[0]
-//     audioEl.play()
-//   }
-//   render() {
+export default class extends Component {
+  playAudio() {
+    const audioEl = document.getElementsByClassName("audio-element")[0]
+    audioEl.play()
+  }
+  render() {
 
-//     return (
-//       <div>
-//         <iframe className="widget" allow="autoplay" width="60%" height="50px" src="https://www.iheart.com/artist/calvin-harris-60352/?embed=true" frameborder="0"></iframe>
-//       </div>
-//     )
-//   }
-// }
+    return (
+      <div>
+        <iframe className="widget" allow="autoplay" width="60%" height="50px" src="https://www.iheart.com/artist/calvin-harris-60352/?embed=true" frameborder="0"></iframe>
+      </div>
+    )
+  }
+}
 
